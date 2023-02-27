dd64e3deb46bae1328f35567ab9fc333ee7dfd79
Delavalom <laarvelo@icloud.com>
2023-02-27 03:39:19 -0400

    update testing, branch ready to deploy for course

--- _test_/Note.test.tsx
+++ _test_/Note.test.tsx
@@ -2,35 +2,26 @@ import { test, describe, expect, vi } from "vitest";
 import { render, screen } from "@testing-library/react";
 import mockRouter from "next-router-mock";
 import Note from "../src/pages/[note]";
-import Link from "next/link";
 
 vi.mock("next/router", () => require("next-router-mock"));
 
 describe("Note", () => {
-  test("Dynamic route should displays correct pathname inside heading asadasda", async () => {
-    mockRouter.push("/?note=asadasda");
+  test("Dynamic route should displays correct pathname inside heading", async () => {
+    mockRouter.push("/?note=first-note");
     render(<Note />);
     const noteTitleElement = screen.getAllByRole("heading", { level: 1 });
-    expect(noteTitleElement[0].textContent).toContain("asadasda");
+    expect(noteTitleElement[0].textContent).toContain("first-note");
   });
-  test("Dynamic route should displays correct pathname inside heading pedro", async () => {
-    mockRouter.push("/?note=pedro");
+  test("Dynamic route should displays correct pathname inside heading", async () => {
+    mockRouter.push("/?note=employees");
     render(<Note />);
     const noteTitleElement = screen.getAllByRole("heading", { level: 1 });
-    expect(noteTitleElement[1].textContent).toContain("pedro");
+    expect(noteTitleElement[1].textContent).toContain("employees");
   });
-  test("Dynamic route should displays correct pathname inside heading juan", async () => {
-    mockRouter.push("/?note=juan");
+  test("Dynamic route should displays correct pathname inside heading", async () => {
+    mockRouter.push("/?note=adadsadsad");
     render(<Note />);
     const noteTitleElement = screen.getAllByRole("heading", { level: 1 });
-    expect(noteTitleElement[2].textContent).toContain("juan");
-  });
-});
-
-vi.mock("next/link", () => <Link href="/"></Link>);
-describe("Link", () => {
-  test("Is Link component present with his attributes?", async () => {
-    render(<Note />);
-    const link = <Link href="/" />;
+    expect(noteTitleElement[2].textContent).toContain("adadsadsad");
   });
 });
--- src/pages/[note].tsx
+++ src/pages/[note].tsx
@@ -1,22 +1,17 @@
-/* eslint-disable @next/next/no-html-link-for-pages */
 import Link from "next/link";
-import { useRouter } from "next/router";
 import { type FC } from "react";
 
 type Props = {};
 
 const Note: FC<Props> = () => {
   // TODO: Extract the query param from useRouter hook
-  const { note } = useRouter().query;
 
   return (
     <section className="center">
       <h1 className="text-4xl font-semibold">
         {/* TODO: Display the query param from the route */}
-        {note}
+        note
       </h1>
-      {/* TODO: Convert the following "a" tag to a Link Component */}
-      {/* <a href="/" className="underline">Go Home</a> */}
       <Link href="/" className="underline">
         Go Home
       </Link>
