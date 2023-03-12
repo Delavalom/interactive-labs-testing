72e6864572bdc79e93155208ba3ea0b32f444d63
Delavalom <laarvelo@icloud.com>
2023-03-11 23:42:52 -0400

    finish tests

--- __labtests/schema.test.ts
+++ __labtests/schema.test.ts
@@ -13,12 +13,37 @@ describe("Validate Prisna Schema", () => {
   test("Does it have datasource?", async () => {
     const data = readFileSync(prismaSchema, "utf-8");
 
-    expect(data.includes('datasource')).toBe(true);
+    expect(data.includes("datasource")).toBe(true);
   });
 
   test("Does it have generator?", async () => {
     const data = readFileSync(prismaSchema, "utf-8");
 
-    expect(data.includes('generator')).toBe(true);
+    expect(data.includes("generator")).toBe(true);
+  });
+
+  test("Does it have models?", async () => {
+    const data = readFileSync(prismaSchema, "utf-8");
+
+    const lines = data.split("\n");
+    expect(
+      lines.filter((line) => line.includes("model")).length
+    ).toBeGreaterThan(0);
+  });
+
+  test("Does the models have an ID?", async () => {
+    const data = readFileSync(prismaSchema, "utf-8");
+    const lines = data.split("\n");
+
+    const ids = lines
+      .map(
+        (line, idx) =>
+          line.includes("model") && lines[idx + 1].replace(/\s+/g, " ").trim()
+      )
+      .filter(Boolean) as string[]
+
+    expect(
+      ids.every((id) => id.includes("id Int @id @default(autoincrement())"))
+    ).toBe(true);
   });
 });
