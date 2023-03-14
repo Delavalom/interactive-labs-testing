9cf1125634de2efd9491d9dc7c2112d32dc15a28
Delavalom <laarvelo@icloud.com>
2023-03-14 18:50:20 -0400

    test ready

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -6,17 +6,4 @@ import { AuthOptions } from "next-auth";
 const clientId = process.env.GITHUB_ID!;
 const clientSecret = process.env.GITHUB_SECRET!;
 
-export const authOptions = {
-  providers: [
-    GithubProvider({
-      clientId,
-      clientSecret,
-    }),
-  ],
-  adapter: PrismaAdapter(prisma),
-  callbacks: {
-    async jwt({ token }) {
-      return token;
-    },
-  },
-} satisfies AuthOptions;
+export const authOptions = {} // satisfies AuthOptions;
