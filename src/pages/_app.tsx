89b66b07f1dbd36e40c635426df1f566e0bf7674
Delavalom <laarvelo@icloud.com>
2023-03-24 15:38:45 -0400

    fix some files

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,6 @@
 import "@/styles/globals.css";
-import { api } from "@/utils/trpc";
-import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
 
-function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
-  return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
-  );
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
 }
-
-export default api.withTRPC(App);
