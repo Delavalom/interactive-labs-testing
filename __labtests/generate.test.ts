5c0e532c453b3643e8b136c4723f2568e8f06df4
Delavalom <laarvelo@icloud.com>
2023-03-24 15:25:28 -0400

    Revert changes

---
+++ __labtests/generate.test.ts
@@ -0,0 +1,32 @@
+import { describe, expect, test } from "vitest";
+import { createMocks } from "node-mocks-http";
+import handler from "../src/pages/api/generate";
+
+describe("Test api generate", () => {
+  test("using a GET method", async () => {
+    const { req, res } = createMocks({
+      method: "GET",
+    });
+
+    await handler(req, res);
+
+    expect(JSON.parse(res._getData())).toEqual({
+      greetings: "Hello world",
+      message: "OK",
+    });
+  });
+
+  test("using a POST method", async () => {
+    const { req, res } = createMocks({
+      method: "POST",
+      body: { name: "Joe" },
+    });
+
+    await handler(req, res);
+
+    expect(JSON.parse(res._getData())).toEqual({
+      greetings: "Hello Joe",
+      message: "OK",
+    });
+  });
+});
--- src/pages/api/generate.ts
+++ src/pages/api/generate.ts
@@ -2,12 +2,21 @@ import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
   message: "OK" | "Internal Server Error" | "Bad Request";
-  greetings?: `Hello ${string}`
+  greetings?: `Hello ${string}`;
 };
 
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === "GET") {
+    return res.status(200).json({ message: "OK", greetings: "Hello world" });
+  } else if (req.method === "POST") {
+    const { name } = req.body;
+    return res.status(200).json({ message: "OK", greetings: `Hello ${name}` });
+  }
+  return;
+}
 
-
-{/* 
+{
+  /* 
   TODO: Create a handler function that recieve GET and POST requests
     
     Validate methods
@@ -16,4 +25,5 @@ type Data = {
     
     if POST is true than you should return a { message: "OK", greetings: `Hello ${name}` } 
 
-*/}
+*/
+}
